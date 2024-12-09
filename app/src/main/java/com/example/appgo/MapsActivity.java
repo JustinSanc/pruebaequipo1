53bed083cbfe1865ee223ba686c019d85f755656
JesusL2 <jesus_s@tesch.edu.mx>
2024-12-09 17:12:23 -0600

    Update MapsActivity.java
    Hola

--- app/src/main/java/com/example/appgo/MapsActivity.java
+++ app/src/main/java/com/example/appgo/MapsActivity.java
@@ -92,6 +92,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Toast.makeText(this, "Permiso de ubicación denegado", Toast.LENGTH_SHORT).show();
                 Toast.makeText(this, "Prueba, Justin", Toast.LENGTH_SHORT).show();
                 Toast.makeText(this, "Prueba, Ari", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Prueba, yisus", Toast.LENGTH_SHORT).show();
             }
         }
     }
